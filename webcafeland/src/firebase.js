0aba3238212d97214305812005983c7dcbffc000
G K <gk@gs-macbook-air.lan>
2025-06-24 17:18:35 -0700

    Firebase connected 2

--- webcafeland/src/firebase.js
+++ webcafeland/src/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDqHicC9wtcveXprHisjF9IX6lY98JAwkk",
-  authDomain: "swiproj.firebaseapp.com",
-  projectId: "swiproj",
-  storageBucket: "swiproj.firebasestorage.app",
-  messagingSenderId: "944992814823",
-  appId: "1:944992814823:web:20948cf6f06a6f5e9ac442",
-  measurementId: "G-5E0LKZGMEY"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
